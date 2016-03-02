191106c283f4d781208d46c6fe22e92b946fb194
Mark Martin <assplecake@gmail.com>
2016-03-02 09:03:12 -0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-APPENV=${APPENV:-spanxenv}
-
-/opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$APPENV > /$APPENV
+APPENV=${APPENV:-stinkbaitenv}
 
 /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$APPENV > /$APPENV
 
 source /$APPENV && \
-  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$SPANX_CERT > /$SPANX_CERT && \
-  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$SPANX_CERT_KEY > /$SPANX_CERT_KEY && \
-  chmod 600 /$SPANX_CERT_KEY && \
-	/opt/bin/migrate -url "$POSTGRES_CONN" -path /migrations up && \
-  /spanx
+  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$STINKBAIT_CERT > /$STINKBAIT_CERT && \
+  /opt/bin/s3kms -r us-west-1 get -b opsee-keys -o dev/$STINKBAIT_CERT_KEY > /$STINKBAIT_CERT_KEY && \
+  chmod 600 /$STINKBAIT_CERT_KEY && \
+  /stinkbait
